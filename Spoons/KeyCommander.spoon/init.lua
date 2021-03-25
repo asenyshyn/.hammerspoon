76e9d755c1505d9db5f810eb3557f85ecb4f2691
Andriy Senyshyn <sam@maytech.net>
2021-03-25 11:50:57 +0200

    cleanup and updates

--- Spoons/KeyCommander.spoon/init.lua
+++ Spoons/KeyCommander.spoon/init.lua
@@ -8,8 +8,9 @@ obj.author = "Andriy Senyshyn <asenyshyn@gmail.com>"
 obj.license = "MIT - https://opensource.org/licenses/MIT"
 
 obj.logger = hs.logger.new("KeyCommander")
+obj.cache = {}
 
-obj.hyperKey = hs.hotkey.modal.new()
+obj.hyperKey = hs.hotkey.modal.new({}, 'F17')
 
 obj.defaultHotkey = {{}, "F18"}
 
@@ -24,20 +25,38 @@ function obj:exitHyperMode()
 end
 
 -- build our own hyper key app launch shortcuts
-function obj:launch(appname)
-  local found = hs.application.launchOrFocus(appname)
-  if found then
-    local app = hs.application.find(appname)
-    if appname == "Finder" then
-      app:selectMenuItem({"Window", "Bring All to Front"})
+function obj:launch(appName)
+  -- first focus with hammerspoon
+  hs.application.launchOrFocus(appName)
+
+  -- clear timer if exists
+  if obj.cache.launchTimer then obj.cache.launchTimer:stop() end
+
+  -- wait 500ms for window to appear and try hard to show the window
+  obj.cache.launchTimer = hs.timer.doAfter(0.5, function()
+    local frontmostApp     = hs.application.frontmostApplication()
+    local frontmostWindows = hs.fnutils.filter(frontmostApp:allWindows(), function(win) return win:isStandard() end)
+
+    if frontmostApp:title() == "Finder" then
+      frontmostApp:selectMenuItem({"Window", "Bring All to Front"})
     end
-    local win = app:mainWindow()
-    if win then
-      win:focus()
+    -- break if this app is not frontmost (when/why?)
+    if frontmostApp:title() ~= appName then
+      print('Expected app in front: ' .. appName .. ' got: ' .. frontmostApp:title())
+      return
     end
-  else
-    self.logger.e(appname .. " not found")
-  end
+
+    if #frontmostWindows == 0 then
+      -- check if there's app name in window menu (Calendar, Messages, etc...)
+      if frontmostApp:findMenuItem({ 'Window', appName }) then
+        -- select it, usually moves to space with this window
+        frontmostApp:selectMenuItem({ 'Window', appName })
+      else
+        -- otherwise send cmd-n to create new window
+        hs.eventtap.keyStroke({ 'cmd' }, 'n')
+      end
+    end
+  end)
 end
 
 function obj:bindHotkeys(mapping)
--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 ---
---- settings
----
-hs.logger.setGlobalLogLevel("info")
-
----
 --- local functions
 ---
 local function activateYubikey()
-  hs.eventtap.keyStrokes(hs.execute('PATH=$HOME/bin:/usr/local/bin:$PATH /Users/sam/bin/yubi/yubi_goog.py --yubi', false))
+  local output, status, exit_type, code = hs.execute('PATH=$HOME/bin:/usr/local/bin:$PATH /Users/sam/bin/yubi/yubi_goog.py --yubi', false)
+  if status then
+    hs.eventtap.keyStrokes(output)
+  else
+    hs.notify.show("Yubikey failed", "failed to execute yubi_goog", "Code: " .. code .. " type: ".. exit_type)
+  end
 end
 
 local function toggleFullscreen()
@@ -28,7 +28,7 @@ local Install=spoon.SpoonInstall
 local wmKey      = {"cmd", "alt"}
 local wmKeyShift = {"cmd", "alt", "shift"}
 
-hs.window.animationDuration = 0
+hs.window.animationDuration = 0.05
 Install:andUse("MiroWindowsManager",
                 {
                   hotkeys = {
@@ -40,16 +40,88 @@ Install:andUse("MiroWindowsManager",
                   }
                 }
 )
+-- spoon.MiroWindowsManager.sizes = { 6/5, 4/3, 3/2, 2/1, 3/1, 4/1, 6/1 }
+
+-- center focused x and y
+hs.hotkey.bind(wmKey, 'c', function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local max = win:screen():frame()
+
+	local x = f
+
+	x.x = ((max.w - f.w) / 2) + max.x
+	x.y = ((max.h - f.h) / 2) + max.y
+	win:setFrame(x)
+end)
+
+-- center focused y
+hs.hotkey.bind(wmKey, 'x', function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local max = win:screen():frame()
+
+	local x = f
+
+	x.y = ((max.h - f.h) / 2) + max.y
+	win:setFrame(x)
+end)
+
+-- center focused x
+hs.hotkey.bind(wmKey, 'v', function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local max = win:screen():frame()
+
+	local x = f
+
+	x.x = ((max.w - f.w) / 2) + max.x
+	win:setFrame(x)
+end)
+
+-- maximize on wide screen
+hs.hotkey.bind(wmKey, 'w', function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local max = win:screen():frame()
+
+  local x = f
+
+  x.y = max.y
+  x.h = max.h
+  x.w = 8 * max.w / 9
+  x.x = ((max.w - f.w) / 2) + max.x
+  win:setFrame(x)
+end)
+
+-- maximize vertically
+hs.hotkey.bind(wmKey, '[', function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local max = win:screen():frame()
+
+	local x = f
+
+	x.y = max.y
+	x.h = max.h
+	win:setFrame(x)
+end)
+
+-- maximize horizontally
+hs.hotkey.bind(wmKey, ']', function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local max = win:screen():frame()
+
+	local x = f
+
+	x.x = max.x
+	x.w = max.w
+	win:setFrame(x)
+end)
 
 Install:andUse("ReloadConfiguration",
                {
-                 start = true
-               }
-)
-
-Install:andUse("TimeMachineProgress",
-               {
-                 disable = true,
                  start = true
                }
 )
@@ -91,10 +163,12 @@ spoon.KeyCommander:bindHotkeys({
       {{}, 'N', 'Notes'},
       {{}, 'P', 'MacPass'},
       {{}, 'T', 'iTerm'},
-      {{}, 'V', 'VirtualBox'},
-      {{}, 'W', 'Safari'},
+      {{}, 'V', 'Viber'},
+      {{}, 'Z', 'Zoom.us.app'},
+      {{}, 'W', 'Firefox'},
       {{}, '.', 'Dash'},
       {{}, ',', 'System Preferences'},
+      {{}, ';', 'Activity Monitor'},
       {{}, '\\', 'Amphetamine'}
     },
     actionKeys = {
@@ -104,3 +178,21 @@ spoon.KeyCommander:bindHotkeys({
       {{"shift"}, "0", hs.openConsole}
     }
 })
+
+local lastPsuSerial = "unknown"
+
+local function batteryChangedCallback()
+  psuSerial = hs.battery.psuSerialString()
+  if hs.battery.powerSource() == "AC Power"  then
+    if psuSerial ~= "C4H701102AMGN8RA4"
+      and psuSerial ~=""
+      and psuSerial ~= lastPsuSerial
+    then
+      hs.notify.show("Battery Watcher", "That's not your power supply!", psuSerial)
+    end
+    lastPsuSerial = psuSerial
+  end
+end
+
+local batteryWatcher = hs.battery.watcher.new(batteryChangedCallback)
+batteryWatcher:start()
